fd9a96ca13df2679d0bdc403412548c94044c208
muhammad-mamdouh99 <muhammad.mamdouh99@gmail.com>
2022-10-11 15:21:07 +0200

    Add expanding-cards fils

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClasses = function () {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+};
